f0512ba31db3e6498a501f039f4b23925b266877
Denis <denis.shagim@icloud.com>
2025-01-08 18:09:15 +0500

    feat: add logic function for search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,36 @@
 import { Component } from "react";
 
 class Search extends Component {
-  state = {
-    search: "",
+  constructor() {
+    super();
+    this.state = {
+      search: "",
+    };
+  }
+
+  handleKey = (event) => {
+    if (event.key === "Enter") {
+      this.props.searchMovies(this.state.search);
+    }
   };
   render() {
     return (
       <div className="row">
         <div className="input-field">
           <input
+            className="validate"
             placeholder="search"
             type="search"
-            className="validate"
             value={this.state.search}
             onChange={(e) => this.setState({ search: e.target.value })}
+            onKeyDown={this.handleKey}
           />
+          <button
+            onClick={() => this.props.searchMovies(this.state.search)}
+            className="btn search-btn"
+          >
+            Search
+          </button>
         </div>
       </div>
     );
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -10,16 +10,20 @@ class Main extends Component {
   };
 
   componentDidMount() {
-    fetch("http://www.omdbapi.com/?s=matrix&apikey=68565c82")
+    fetch(`http://www.omdbapi.com/?s=matrix&apikey=68565c82`)
       .then((response) => response.json())
       .then((data) => this.setState({ movies: data.Search, loading: false }));
   }
-
+  searchMovies = (str) => {
+    fetch(`http://www.omdbapi.com/?s=${str}&apikey=68565c82`)
+    .then((response) => response.json())
+    .then((data) => this.setState({ movies: data.Search, loading: false }));
+  }
   render() {
     const { movies } = this.state;
     return (
       <main className="container content">
-        <Search />
+        <Search searchMovies={this.searchMovies}/>
         {movies.length ? <Movies movies={movies} /> : <Preloader />}
       </main>
     );
