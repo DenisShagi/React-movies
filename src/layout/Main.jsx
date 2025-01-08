b66907c59c04122b9a5e344006ffc9602635553c
Denis <denis.shagim@icloud.com>
2025-01-08 17:29:24 +0500

    feat: Added rendering of movie cards and request to the service

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,6 +1,29 @@
-function Main(){
+import { Component } from "react";
+import { Movies } from "../components/Movies";
+
+class Main extends Component {
+  state = {
+    movies: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    fetch("http://www.omdbapi.com/?s=matrix&apikey=68565c82")
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search, loading: false }));
+  }
+
+  render() {
+    const { movies } = this.state;
     return (
-        <main></main>
-    )
+      <main className="container content">
+        {movies.length ? (
+          <Movies movies={movies} />
+        ) : (
+          <h3>Loading...</h3>
+        )}
+      </main>
+    );
+  }
 }
-export {Main}
+export { Main };
