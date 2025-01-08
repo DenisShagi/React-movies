1065970a92dc36ca3159c5becffe664476b86e49
Denis <denis.shagim@icloud.com>
2025-01-08 16:35:42 +0500

    added header and footer for the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import "./App.css";
+import { Header } from "./layout/Header";
+import { Footer } from "./layout/Footer";
+import { Main } from "./layout/Main";
 
 function App() {
   return (
     <>
- 
+      <Header />
+      <Footer />
+      <Main />
     </>
   );
 }
